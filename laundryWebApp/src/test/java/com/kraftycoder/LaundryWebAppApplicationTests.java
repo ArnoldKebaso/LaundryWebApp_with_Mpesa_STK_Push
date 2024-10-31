5a2eb58acc1df81549539e641c1428681dfcc917
ArnoldKebaso <arnoldkebaso8@gmail.com>
2024-10-31 14:18:06 +0300

    feat:created a springboot folder00

---
+++ laundryWebApp/src/test/java/com/kraftycoder/LaundryWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kraftycoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaundryWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
